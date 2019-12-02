e60525b6580fe096bbdbcd9d45d942ae4fd14020
Sandeep Bishnoi <sandeep.bishnoi@workday.com>
2019-12-02 10:52:56 -0800

    Day-2 Solutions

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -18,7 +18,7 @@ import scala.io.Source
   * For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
   * For a mass of 1969, the fuel required is 654.
   * For a mass of 100756, the fuel required is 33583.
-  * The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
+  * The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle day1_input), then add together all the fuel values.
   *
   * What is the sum of the fuel requirements for all of the modules on your spacecraft?
   *
@@ -29,7 +29,7 @@ import scala.io.Source
   *
   * Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
   *
-  * So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+  * So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the day1_input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
   *
   * A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
   * At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
@@ -45,7 +45,7 @@ object Day1 {
   }
 
   def part1(): Long = {
-    val masses = "input"
+    val masses = "day1_input"
     var fuel = 0L
     for(mass <- Source.fromResource(masses).getLines()) {
       fuel = fuel + (math.floor(mass.toInt/3).toLong - 2)
@@ -54,7 +54,7 @@ object Day1 {
   }
 
   def part2(): Long = {
-    val masses = "input"
+    val masses = "day1_input"
     var fuel = 0L
     for(mass <- Source.fromResource(masses).getLines()) {
       var currMass = mass.toLong
